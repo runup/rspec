62313131824aafcb4796be6339c4656bcca9b339
runup <645112281@qq.com>
2014-03-17 23:33:38 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Setup::Application.config.secret_key_base = '517115100a079c44095cc07a68f7c310b3b4bfd86c629d64b9b3cd707ec16699bf34eeb887f8d56699e2b69e77e7d6d46e602177ee27b711ef36b7dd90d6f4ed'
---
+++ spec/factories/contacts.rb
@@ -0,0 +1,10 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :contact do
+    firstname "MyString"
+    lastname "MyString"
+    email "MyString"
+    phones "MyString"
+  end
+end
